1726c3e2156e370df9de0af0d4631da224b2d967
Bharathhh30 <vnsb30@gmail.com>
2025-05-25 13:41:01 +0530

    routing done

--- 8.1/index.js
+++ 8.1/index.js
@@ -1,7 +1,8 @@
 const express = require("express");;
 const jwt = require("jsonwebtoken");
-const { createUser, userRouter } = require("./routes/user");
-const { createCourse, courseRouter } = require("./routes/course");
+const {  userRouter } = require("./routes/user");
+const {  courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 
 const app = express()
 
@@ -12,6 +13,7 @@ const app = express()
 
 // industry way
 app.use("/api/v1/user",userRouter)
-app.use("/api/v1 /course",courseRouter)
+app.use("api/v1/admin",adminRouter)
+app.use("/api/v1/course",courseRouter)
 
 app.listen(3000);
---
+++ 8.1/routes/admin.js
@@ -0,0 +1,36 @@
+const {Router} = require("express")
+const adminRouter = Router()
+
+adminRouter.post("/signup",(req,res)=>{
+    res.json({
+        message : "signup endpoint"
+    })
+})
+
+adminRouter.post("/signin",(req,res)=>{
+    res.json({
+        message : "signin endpoint"
+    })
+})
+
+
+adminRouter.post("/course",(req,res)=>{
+    res.json({
+        message : "course end pint"
+    })
+})
+
+adminRouter.put("/course",(req,res)=>{
+    res.json({
+        message : "course end pint"
+    })
+})
+
+adminRouter.get("/course/bulk",(req,res)=>{
+    res.json({
+        message : "course end pint"
+    })
+})
+module.exports = {
+    adminRouter : adminRouter
+}
